29f827365d141cc3d13e4e65a5dd24aaf683a2c2
Towa Yokoyama <soccertowa0923@outlook.com>
2025-10-08 12:56:04 +0900

    comment day 10

--- day10.py
+++ day10.py
@@ -6,8 +6,9 @@ class Solution:
         is_prime = [True] * n  # n未満の数
         is_prime[0] = is_prime[1] = False  # 0,1は素数でない
 
+#テストケースをn=10やとして、√10＝3.16...位を考慮
         for i in range(2, int(n ** 0.5) + 1):
-            if is_prime[i]:
+            if is_prime[i]: #i = 2と3に決まる
                 # iの倍数をFalseにしていく
                 for j in range(i * i, n, i):
                     is_prime[j] = False
