b588e8a2a42a32ac8c36a1c6fb76b7c6e225b97a
Towa Yokoyama <soccertowa0923@outlook.com>
2025-10-09 18:14:18 +0900

    day15

--- day14.py
+++ day14.py
@@ -27,3 +27,14 @@ class Solution:
             for j in range(i+1,len(nums)):
                 if nums[j] + nums[i] == target:
                     return(i,j)
+                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+            seen = {}
+            for i, num in enumerate(nums): 
+                complement = target - num
+                if complement in seen:
+                    return [seen[complement], i]
+                seen[num] = i
+                
+                
