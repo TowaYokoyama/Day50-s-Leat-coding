95ea54ab4510c401c57631544237f6226a481a26
Towa Yokoyama <soccertowa0923@outlook.com>
2025-10-10 19:02:50 +0900

    day17

--- day16.py
+++ day16.py
@@ -28,6 +28,20 @@ n == nums.length
         for num,count in seen.items():
             if count > n //2:
                 return num  
+            
+            
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
+
         
         
         
