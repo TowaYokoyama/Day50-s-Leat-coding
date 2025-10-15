8d235094ffc657d14b48c58438b83f10c5b21b34
Towa Yokoyama <soccertowa0923@outlook.com>
2025-10-15 16:23:16 +0900

    day29

--- day28.py
+++ day28.py
@@ -29,3 +29,17 @@ class Solution:
             
         dfs(0,[])
         return res
+    
+class Solution:
+    def subsets(self, nums:List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        
+        for mask in range(1<<n):
+            subset = []
+            for i in range(n):
+                if mask & (1 << i):
+                    subset.append(nums[i])
+            res.append(subset)
+        
+        return res
