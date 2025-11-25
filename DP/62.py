f879beb30132b7e20b90cfa7055b734d87cfd6df
Towa Yokoyama <191555241+TowaYokoyama@users.noreply.github.com>
2025-11-25 22:00:46 +0900

    62

--- DP/62.py
+++ DP/62.py
@@ -21,5 +21,33 @@ Example 2:
 
 Input: obstacleGrid = [[0,1],[0,0]]
 Output: 1
- 
 """
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0]) # 行と列の数
+        
+        # スタート地点が障害物なら絶対に無理
+        if obstacleGrid[0][0] == 1:
+            return 0
+        
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+
+        for i in range(m):
+            for j in range(n):
+                # スタート点はすでに1でセット済み
+                if i == 0 and j == 0:
+                    continue
+
+                # 障害物なら通れない
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    # 上から来れる？
+                    if i > 0:
+                        dp[i][j] += dp[i-1][j]
+                    # 左から来れる？
+                    if j > 0:
+                        dp[i][j] += dp[i][j-1]
+
+        return dp[m-1][n-1]
